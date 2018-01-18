f9bc0cf4f329a90720245a50236bbddcec432733
AshishRawandhe <rawandheashish111@gmail.com>
2018-01-18 16:51:00 +0530

    added changes for EVS service client method

--- openstack/client.go
+++ openstack/client.go
@@ -337,6 +337,18 @@ func NewVpcV1(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (
 	sc.ResourceBase = sc.Endpoint + "v1/" + pid + "/"
 	return sc, err
 }
+// NewEvsV2 creates a ServiceClient that may be used with the v1 VPC for OTC.
+func NewEvsV2(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
+
+	pid, e := GetProjectId(client)
+	if e != nil {
+		return nil, e
+	}
+
+	sc, err := initClientOpts(client, eo, "network")
+	sc.ResourceBase = sc.Endpoint + "v2/" + pid + "/"
+	return sc, err
+}
 
 // NewOtcV1 creates a ServiceClient that may be used with the v1 network package.
 func NewOtcV1(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts, otctype string) (*gophercloud.ServiceClient, error) {
